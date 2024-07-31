1b422269a5511f39a0aa2ea787f100568a7b0f78
OmarElgaafary <omarelgaafaryy@gmail.com>
2024-08-01 00:40:28 +0300

    Update bubbles.cpp

--- bubbles.cpp
+++ bubbles.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
 
+
+void funcprint(int array[], int length);
+void BubbleDo(int arr[], int n);
+
+
+
+
+
+int main() {
+	
+
+	int a[10] = { 5, 2, 6, 4, 3, 1,10 ,6 ,4 , 7 };
+
+	BubbleDo(a, 10);
+
+}
+
 void funcprint(int array[], int length) {
 	std::cout << "[";
 	for (int i = 0; i < length; i++) {
@@ -16,36 +33,23 @@ void funcprint(int array[], int length) {
 void BubbleDo(int arr[], int n) {
 	bool swapped;
 	int counter = 0;
-	int unsorted = n - 1;
-	int pivot = 0;
 	do {
 		swapped = false;
-		for (int i = 0; i < unsorted; i++) {
+		for (int i = 0; i < n - 1; i++) {
 			if (arr[i] > arr[i + 1]) {
-				// swap
 				int temp = arr[i];
 				arr[i] = arr[i + 1];
 				arr[i + 1] = temp;
 				swapped = true;
-				pivot = i;
 			}
-			
-		} 
-		if (swapped){
+
+		}
+		if (swapped) {
 			counter++;
-			unsorted = pivot;
 			funcprint(arr, n);
 			std::cout << "passes #" << counter;
 			std::cout << std::endl;
 		}
 
 	} while (swapped);
-}
-
-
-int main() {
-	int a[10] = { 5, 2, 6, 4, 3, 1,10 ,6 ,4 , 7 };
-
-	BubbleDo(a, 10);
-
 }
